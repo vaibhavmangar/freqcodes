622e26b2aa8bf8c806972098db513ca3ed8292ce
vaibhav mangar <95011943+vaibhavmangar@users.noreply.github.com>
2024-09-04 09:17:28 +0530

    Update linear.m
    erors removed

--- linear.m
+++ linear.m
@@ -15,9 +15,9 @@ title('/p sequence x(n)is:');
 xlabel('---->n');
 ylabel('---->x(n)');grid;
 y=zeros(1,m+n-1);
-for i=1:n
-    for j=1:m
-        y(i+j)=y(i+j)+x(i)*h(j);
+for i=1:m
+    for j=1:n
+        y(i+j-1)=y(i+j-1)+x(i)*h(j);
     end
 end
 y
@@ -25,10 +25,3 @@ subplot(2,2,[3,4]),stem(y);
 title("convolution of x(n) & h(n) is :");
 xlabel('---->n');
 ylabel('---->y(n)');grid;
-
-
-
-
-
-
-
